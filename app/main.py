649e8067b07565fe7063a5274c6e37bc40a35129
Vitalik <lg.mobile0808@gmail.com>
2025-04-13 18:01:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,13 @@
-# write youcxr code here
-bo =3
+def copy_file(command: str) -> None:
+    parts = command.strip().split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, src, dest = parts
+
+    if src == dest:
+        return
+
+    with open(src, "r") as file_in, open(dest, "w") as file_out:
+        file_out.write(file_in.read())
